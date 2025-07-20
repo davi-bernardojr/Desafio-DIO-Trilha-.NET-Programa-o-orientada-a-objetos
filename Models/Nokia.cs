f5eb6a9bbac45dbc320eb7deeaa6707868215276
Davi Bernardo Jr <davi.bernardo.db@gmail.com>
2025-07-19 21:45:25 -0300

    Final commit

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // Implementado
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+        // Implementado
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Baixando {nomeApp} da Google Play...");
+            Console.WriteLine($"Instalando {nomeApp}...");
+            Console.WriteLine($"{nomeApp} instalado com sucesso.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone n = new Nokia("123456", "c99", "125478", 64);
+n.Ligar();
+n.InstalarAplicativo("Jogos");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone i = new Iphone("123456", "c99", "125478", 64);
+i.ReceberLigacao();
+i.InstalarAplicativo("Jogos");
